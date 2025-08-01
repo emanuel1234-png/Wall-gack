b59250b5d514478eda0a9dba75be1217e1db26ee
emanuel1234-png <Alessandropiao94@gmail.com>
2025-08-01 19:56:54 -0300

    Update and rename README.md to README.lua

---
+++ README.lua
@@ -0,0 +1,8 @@
+-- Roblox Noclip Script com Botão Flutuante para Delta Mobile -- Criado por TigrinhoMods - Educacional
+
+-- Criar botão flutuante local ScreenGui = Instance.new("ScreenGui") local ToggleButton = Instance.new("TextButton")
+
+ScreenGui.Name = "NoclipGui" ScreenGui.Parent = game.CoreGui
+
+ToggleButton.Name = "ToggleButton" ToggleButton.Parent = ScreenGui ToggleButton.BackgroundColor3 = Color3.fromRGB(30, 30, 30) ToggleButton.BorderSizePixel = 0 ToggleButton.Position = UDim2.new(0, 10, 0, ... }
+
